af6285d218c9f57f63f97d131292cacb1cd3e026
adityaverma47 <130073406+adityaverma47@users.noreply.github.com>
2024-02-13 09:51:56 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MedCab Task2',
+      title: 'MedCab Task ',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -38,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("MedCab Task"),
+        title: Text("MedCab Task 2"),
         centerTitle: true,
       ),
       body: Padding(
